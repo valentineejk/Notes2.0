0214e8a02edf33a0863a3d35cd11eb8c86865322
valentineejk <valentineejk@gmail.com>
2022-11-18 12:07:28 +0100

    Initial Commit

---
+++ Notes2.0/Notes2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Notes2_0App.swift
+//  Notes2.0
+//
+//  Created by Valentineejk on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Notes2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
